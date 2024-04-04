1a2e825c2733012c114e27f5731de43036fa999c
Erik <rainer.erik70@gmail.com>
2024-04-04 09:53:27 -0700

    start creating functions in the javascript file, change the key for the id in the localstorage because i already have one, start the other functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,34 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    let task = localStorage.setItem("taskID", JSON.stringify(crypto.randomUUID()));
+    let task = localStorage.setItem("nextID", JSON.stringify(crypto.randomUUID()));
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    let taskID = JSON.parse(localStorage.getItem("taskID"));
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    if(taskList === null){
+        taskList = [];
+    }
+    else{
+        const todoList = $("todo-cards");
+        todoList.empty();
 
+        const inProgressList = $("#in-progress-cards")
+        inProgressList.empty();
+
+        const doneList = $("#done-cards");
+        doneList.empty();
+
+        for(let i = 0; i < taskList.length, i++){
+            if(taskList[i].status === "to-do"){
+                todoList.append(createTaskCard(taskList[i]));
+            }
+        }
+    }
 }
 
 // Todo: create a function to handle adding a new task
@@ -34,5 +51,11 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    renderTaskList();
+    $("#add-task").on("click", handleAddTask);
+    $(".task-list").on("click", ".delete", handleDeleteTask);
+    $(".lane").droppable({
+        drop: handleDrop();
+    });
+    $("#due-date").datepicker();
 });
