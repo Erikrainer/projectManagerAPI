5a896bcaaf7aed3555567d300295b357f2c7873b
Erik <rainer.erik70@gmail.com>
2024-04-04 15:08:08 -0700

    create the function handleDrop, everything looks working fine no test yet, i didnt create the function to show the task yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,7 +99,7 @@ const task = {
     dueDate: $("#taskDueDate").val(),
 
     status: "to-do"
-    
+
 }
 
 taskList.push(task);
@@ -124,15 +124,36 @@ function handleDeleteTask(event){
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
 
+    const taskID = ui.draggable.dataset.taskId;
+
+    const newStatus = event.target.id
+
+    for(let i = 0; i < taskList.length; i++){
+        if(taskList[i].id == parseInt(taskID)){
+            taskList[i].status = newStatus;
+        }
+    }
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    renderTaskList();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+
     renderTaskList();
+
     $("#taskForm").on("submit", handleAddTask);
+
     $(".task-list").on("click", ".delete", handleDeleteTask);
-    $(".lane").droppable({
+
+    $(".lane").draggable({
+
+        accept: ".draggable",
+
         drop: handleDrop(),
+
     });
+
     $("#due-date").datepicker();
+
 });
